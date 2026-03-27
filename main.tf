1db7402b99abc219d799e06b3d29111339bf5626
Mimid BEN <mimidben@Mimids-MacBook-Pro.local>
2026-03-27 11:16:27 +0100

    Initialisation TP AWS avec variables EC2 et S3

---
+++ main.tf
@@ -0,0 +1,15 @@
+provider "aws" {
+  region                      = "eu-west-1"
+  access_key                  = "mock_access_key"
+  secret_key                  = "mock_secret_key"
+  
+  # Ces options empêchent Terraform de contacter les serveurs AWS
+  skip_credentials_validation = true
+  skip_requesting_account_id  = true
+  skip_metadata_api_check     = true
+  
+  # On lui donne un point de contact imaginaire pour éviter le 403
+  endpoints {
+    sts = "http://localhost:4566"
+  }
+}
---
+++ variables.tf
@@ -0,0 +1,23 @@
+variable "ec2_instance_type" {
+  description = "Le type d'instance EC2"
+  type        = string
+  default     = "t2.micro"
+}
+
+variable "ec2_instance_name" {
+  description = "Le nom de l'instance EC2"
+  type        = string
+  default     = "MonInstanceAWS"
+}
+
+variable "s3_bucket_name" {
+  description = "Le nom du bucket S3 (doit être unique)"
+  type        = string
+  default     = "mon-bucket-terraform-mimid-2026"
+}
+
+variable "sg_port" {
+  description = "Le port par défaut pour le groupe de sécurité"
+  type        = number
+  default     = 80
+}
