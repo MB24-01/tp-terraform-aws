cfe9db4339222067325e9e297e4c4249d9bb555e
Mimid BEN <mimidben@Mimids-MacBook-Pro.local>
2026-04-19 19:09:44 +0200

    feat: exercice 5 - upload de fichier test-file.txt sur S3

--- main.tf
+++ main.tf
@@ -5,3 +5,13 @@ provider "aws" {
 resource "aws_s3_bucket" "demo_bucket" {
   bucket = "lab-s3-mimid-bennani-2026-v5"
 }
+
+
+resource "aws_s3_object" "upload_test" {
+  bucket = aws_s3_bucket.demo_bucket.id # On fait référence au bucket créé plus haut
+  key    = "test-file.txt"              # Le nom du fichier une fois dans S3
+  source = "test-file.txt"              # Le chemin vers ton fichier sur ton Mac
+  
+  # Optionnel mais recommandé : permet de voir si le contenu a changé
+  etag = filemd5("test-file.txt")
+}
